8a85b641ef63f1d841c4dd5a113a2f15204a2ed4
pihu <pihuc250@gmail.com>
2024-09-04 11:32:25 +0530

    3rd  commit

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req,res) =>{
+    res.json({
+        message: 'welcome to mern stack .Our Api is workkng'
+    })
+}
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+const app = express();
+const router = express.Router();
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from'dotenv';
+import userroutes from './routes/user.js';//routes
+//import authroutes from './routes/auth.route.js';//routes
 dotenv.config();
-//mongoose.connect(process.env.MONGODB_URL)
+
 const mongoURL= 'mongodb://localhost:27017/Projauth';
 mongoose.connect(mongoURL,{ })
     .catch((error) => {
@@ -10,6 +14,7 @@ mongoose.connect(mongoURL,{ })
         process.exit(1);
       });
 const db = mongoose.connection;
+
 db.on("connected",()=>{
     console.log('connected to MongoDb server');
 })
@@ -19,11 +24,22 @@ db.on("disconnected",()=>{
 db.on("error",(error)=>{
     console.log('error in MongoDb server',error);
 })
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use('/backend/user', userroutes);//userroutes use
+//app.use('/backend/auth',authroutes);// authroutes use
+//middleware
+// app.use((err,req,res,next)=>{
+//     const statusCode =err.statusCode || 500;
+//     const message= err.message || 'internal Server Error';
+//     return res.status(statusCode).json({
+//         success: false,
+//         message,
+//         statusCode
+//     });
+//     next();
+// }); 
 
-
-
-
-const app = express();
-app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+app.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
 });
